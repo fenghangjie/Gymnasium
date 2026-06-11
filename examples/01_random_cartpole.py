60be0b295aa9ee8e234029314a310cd5d11eaa44
fenghangjie <hangjie_feng@dnt.com.cn>
2026-06-11 14:59:45 +0800

    打印空格

--- examples/01_random_cartpole.py
+++ examples/01_random_cartpole.py
@@ -8,7 +8,7 @@ def main() -> None:
 
     observation, info = env.reset(seed=42)
     total_reward = 0.0
-
+    print()
     print("Observation space:", env.observation_space)
     print("Action space:", env.action_space)
     print("Initial observation:", observation)
